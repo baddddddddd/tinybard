47d7d491dcefb927fc9ad4c3012fc96050f0793a
baddddddddd <eluxeofficial@gmail.com>
2025-07-19 14:09:17 +0800

    Add stride to dataset traversal

--- tinybard/datasets/the_echo_chamber/the_echo_chamber_dataset.py
+++ tinybard/datasets/the_echo_chamber/the_echo_chamber_dataset.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset
 
 
 class TheEchoChamberDataset(Dataset):
-    def __init__(self, seq_len: int, tokenizer):
+    def __init__(self, tokenizer, seq_len: int, stride: int):
         module_dir = pathlib.Path(__file__).resolve().parent
         raw_dataset = module_dir / "raw.txt"
 
@@ -13,12 +13,14 @@ class TheEchoChamberDataset(Dataset):
             self.raw_text = f.read()
 
         self.seq_len = seq_len
+        self.stride = stride
         self.encoded = tokenizer.encode(self.raw_text)
 
     def __len__(self):
-        return len(self.encoded) - self.seq_len
+        return (len(self.encoded) - self.seq_len) // self.stride
 
     def __getitem__(self, idx):
+        idx *= self.stride
         x = torch.LongTensor(self.encoded[idx : idx + self.seq_len])
         y = torch.LongTensor(self.encoded[idx + 1 : idx + 1 + self.seq_len])
 
