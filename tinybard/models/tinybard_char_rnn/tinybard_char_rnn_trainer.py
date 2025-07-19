c119a1772963b2e28d2727c31c63bf7c73f76c0d
baddddddddd <eluxeofficial@gmail.com>
2025-07-19 12:03:16 +0800

    Save and load rng state in model checkpoints

--- tinybard/models/tinybard_char_rnn/tinybard_char_rnn_trainer.py
+++ tinybard/models/tinybard_char_rnn/tinybard_char_rnn_trainer.py
@@ -1,7 +1,9 @@
 import json
 import pathlib
 import os
+import random
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -52,6 +54,12 @@ class TinyBardCharRnnTrainer:
         self.train_dataset = train_dataset
         self.eval_dataset = eval_dataset
 
+        self.dataloader = DataLoader(
+            self.train_dataset,
+            batch_size=self.args.train_batch_size,
+            shuffle=True,
+        )
+
         self.optimizer = optim.AdamW(
             model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay
         )
@@ -71,22 +79,29 @@ class TinyBardCharRnnTrainer:
                 torch.load(optimizer_file, map_location=self.device)
             )
 
-            trainer_state_file = checkpoint_folder / "trainer_state.json"
-            with open(trainer_state_file, "r") as f:
-                json_string = f.read()
-                trainer_state_dict = json.loads(json_string)
+            trainer_state_file = checkpoint_folder / "trainer_state.pth"
+            trainer_state = torch.load(trainer_state_file, weights_only=False)
 
-            start_epoch = trainer_state_dict["epoch_idx"]
-            step_count = trainer_state_dict["optimizer_steps"]
+            torch.set_rng_state(trainer_state["torch_rng_state"])
+            torch.cuda.set_rng_state_all(trainer_state["cuda_rng_state"])
+            np.random.set_state(trainer_state["numpy_rng_state"])
+            random.setstate(trainer_state["python_rng_state"])
+
+            if trainer_state["dataloader_state"] and hasattr(
+                self.dataloader.sampler, "load_state_dict"
+            ):
+                self.dataloader.sampler.load_state_dict(
+                    trainer_state["dataloader_state"]
+                )
+
+            start_epoch = trainer_state["epoch"]
+            resume_batch_idx = trainer_state["batch_idx"] + 1
+            step_count = trainer_state["steps"]
+
         else:
             start_epoch = 0
+            resume_batch_idx = 0
             step_count = 0
-
-        dataloader = DataLoader(
-            self.train_dataset,
-            batch_size=self.args.train_batch_size,
-            shuffle=True,
-        )
 
         self.model.train()
         for epoch_idx in range(start_epoch, self.args.num_train_epochs):
@@ -95,7 +110,10 @@ class TinyBardCharRnnTrainer:
 
             total_loss = 0
             loss_steps = 0
-            for batch_idx, sample in enumerate(dataloader):
+            for batch_idx, sample in enumerate(self.dataloader):
+                if batch_idx < resume_batch_idx:
+                    continue
+
                 inputs, targets = sample
                 inputs = inputs.to(self.device)
                 targets = targets.to(self.device)
@@ -119,7 +137,7 @@ class TinyBardCharRnnTrainer:
 
                 if self.args.save_steps > 0 and (
                     step_count % self.args.save_steps == 0
-                    or (batch_idx + 1) == len(dataloader)
+                    or (batch_idx + 1) == len(self.dataloader)
                 ):
                     n = len(self.train_dataset)
                     cur = min((batch_idx + 1) * self.args.train_batch_size, n)
@@ -131,9 +149,31 @@ class TinyBardCharRnnTrainer:
 
                     print(f"[{cur:>{width}d}/{n}] avg_loss={avg_loss:.6f}")
 
-                    self.save_checkpoint(step_count, epoch_idx)
+                    self.save_checkpoint(epoch_idx, batch_idx, step_count)
 
-    def save_checkpoint(self, step, epoch_idx):
+    def get_state_dict(self, epoch, batch_idx, step):
+        state_dict = {
+            "epoch": epoch,
+            "batch_idx": batch_idx,
+            "steps": step,
+            "torch_rng_state": torch.get_rng_state(),
+            "cuda_rng_state": torch.cuda.get_rng_state_all(),
+            "numpy_rng_state": np.random.get_state(),
+            "python_rng_state": random.getstate(),
+            "dataloader_state": (
+                self.dataloader.sampler.state_dict()
+                if hasattr(self.dataloader.sampler, "state_dict")
+                else None
+            ),
+        }
+
+        return state_dict
+
+    def save_state(self, epoch, batch_idx, step, filename):
+        state_dict = self.get_state_dict(epoch, batch_idx, step)
+        torch.save(state_dict, filename)
+
+    def save_checkpoint(self, epoch, batch_idx, step):
         folder_name = f"checkpoint-{step}"
         folder_path = self.args.output_dir / folder_name
 
@@ -144,12 +184,5 @@ class TinyBardCharRnnTrainer:
         optimizer_file = folder_path / "optimizer.pth"
         torch.save(self.optimizer.state_dict(), optimizer_file)
 
-        trainer_state_dict = {
-            "optimizer_steps": step,
-            "epoch_idx": epoch_idx,
-        }
-
-        json_string = json.dumps(trainer_state_dict)
-        trainer_state_file = folder_path / "trainer_state.json"
-        with open(trainer_state_file, "w") as f:
-            f.write(json_string)
+        trainer_state_file = folder_path / "trainer_state.pth"
+        self.save_state(epoch, batch_idx, step, trainer_state_file)
