6e18b03483b9783b117e9f6f1d611b7973ba132c
baddddddddd <eluxeofficial@gmail.com>
2025-07-20 20:36:42 +0800

    Add model size calculation in BaseModel

--- tinyfriend/models/base_model.py
+++ tinyfriend/models/base_model.py
@@ -3,4 +3,8 @@ import torch.nn as nn
 
 
 class BaseModel(nn.Module):
-    pass
+    def __init__(self):
+        super().__init__()
+
+    def size(self):
+        return sum(p.numel() for p in self.parameters())
