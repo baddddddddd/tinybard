37bff2e4f5a17d4344cd3e89db612b2c5b207a9d
baddddddddd <eluxeofficial@gmail.com>
2025-07-20 21:31:01 +0800

    Add TrainingConfig

---
+++ tinyfriend/trainer/training_config.py
@@ -0,0 +1,25 @@
+import pathlib
+
+
+class TrainingConfig:
+    def __init__(
+        self,
+        output_dir: str,
+        num_train_epochs: int,
+        learning_rate: float,
+        weight_decay: float,
+        train_batch_size: int,
+        save_strategy: str | None = None,
+        save_steps: int | None = None,
+        eval_batch_size: int | None = None,
+        eval_strategy: str | None = None,
+    ):
+        self.output_dir = pathlib.Path(output_dir).resolve()
+        self.num_train_epochs = num_train_epochs
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+        self.train_batch_size = train_batch_size
+        self.save_strategy = save_strategy
+        self.save_steps = save_steps
+        self.eval_batch_size = eval_batch_size
+        self.eval_strategy = eval_strategy
