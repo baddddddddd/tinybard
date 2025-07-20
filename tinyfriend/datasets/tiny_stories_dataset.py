7bd2b68234e5e7cadec1b22827c03ef2c9446cc1
baddddddddd <eluxeofficial@gmail.com>
2025-07-20 19:50:09 +0800

    Add pad support for TinyStoriesDataset

---
+++ tinyfriend/datasets/tiny_stories_dataset.py
@@ -0,0 +1,68 @@
+import os
+
+import datasets
+import torch
+
+from ..tokenizers import BaseTokenizer
+
+
+class TinyStoriesDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        split: str,
+        tokenizer: BaseTokenizer,
+        chunk_size: int,
+        stride: int | None = None,
+    ):
+        if stride is None:
+            stride = chunk_size
+
+        self.tokenizer = tokenizer
+        self.chunk_size = chunk_size
+        self.stride = stride
+
+        raw_dataset = datasets.load_dataset("roneneldan/TinyStories", split=split)
+
+        def encode(example):
+            sequences = [text + tokenizer.eos_token for text in example["text"]]
+            token_ids = tokenizer(sequences)
+            return {"input_ids": token_ids}
+
+        num_proc = os.cpu_count()
+        self.dataset = raw_dataset.map(
+            encode,
+            remove_columns=["text"],
+            batched=True,
+            num_proc=num_proc,
+        )
+
+        self.idx_to_doc = []
+        for row_idx in range(len(self.dataset)):
+            token_ids = self.dataset[row_idx]["input_ids"]
+            rem = (len(token_ids) - chunk_size) % stride
+            if rem > 1:
+                pad_size = chunk_size - rem + 1
+                token_ids += [tokenizer.pad_token_id] * pad_size
+                self.dataset[row_idx]["input_ids"] = token_ids
+
+            for start_idx in range(0, len(token_ids) - chunk_size, stride):
+                doc = (row_idx, start_idx)
+                self.idx_to_doc.append(doc)
+
+                ids = token_ids[start_idx : start_idx + chunk_size]
+                n_ids = token_ids[start_idx + 1 : start_idx + 1 + chunk_size]
+
+    def __len__(self):
+        return len(self.idx_to_doc)
+
+    def __getitem__(self, idx):
+        row_idx, start_idx = self.idx_to_doc[idx]
+        row = self.dataset[row_idx]
+        token_ids = row["input_ids"]
+
+        inputs = torch.LongTensor(token_ids[start_idx : start_idx + self.chunk_size])
+        targets = torch.LongTensor(
+            token_ids[start_idx + 1 : start_idx + 1 + self.chunk_size]
+        )
+
+        return inputs, targets
